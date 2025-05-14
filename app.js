7d49519fa93ab58cc409da367c15612a9c8f6d62
Mateen Nawaz <mateenNawaz86>
2025-05-14 17:31:29 +0500

    fix: implement user and event models with GraphQL integration

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
+const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose");
+const Event = require("./models/event");
+const User = require("./models/user");
+const bcrypt = require("bcryptjs");
 
 const app = express();
 app.use(bodyParser.json());
-
-const events = [];
 
 app.use(
   "/graphql",
@@ -20,19 +22,32 @@ app.use(
         date: String!
       }
 
-      input EventInput {
+       input EventInput {
         title: String!
         description: String!
         price: Float!
         date: String!
       }
 
+      type User {
+        _id: ID!
+        email: String!
+        password:String
+      }
+
+      input UserInput {
+        email:String!
+        password:String
+      }
+
     type RootQuery {
         events: [Event!]!
+        users:[User!]!
     }
 
     type RootMutation {
         createEvent(eventInput: EventInput): Event 
+        createUser(userInput: UserInput): User
     }
 
     schema {
@@ -43,20 +58,60 @@ app.use(
 
     rootValue: {
       events: () => {
-        return events;
+        Event.find()
+          .then((events) => {
+            return events.map((event) => {
+              return { ...event._doc, _id: event.id.toString() };
+            });
+          })
+          .catch((err) => {
+            console.log("Error fetching events", err);
+            throw err;
+          });
       },
       createEvent: (args) => {
-        const event = {
-          _id: Math.random().toString(),
+        const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          // date: new Date(args.date).toISOString(),
-          date: args.eventInput.date,
-        };
+          date: new Date(args.eventInput.date),
+        });
 
-        events.push(event);
-        return event;
+        return event
+          .save()
+          .then((result) => {
+            return { ...result._doc, _id: result._doc._id.toString() };
+          })
+          .catch((err) => {
+            console.log("Error saving event", err);
+            throw err;
+          });
+      },
+
+      createUser: (args) => {
+        // check that if user with this already email already exist
+        return User.findOne({ email: args.userInput.email })
+          .then((user) => {
+            if (user) {
+              throw new Error("User with this email already exist.");
+            }
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+          .then((hashedPassword) => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword,
+            });
+
+            return user.save();
+          })
+          .then((result) => {
+            return { ...result._doc, password: null, _id: result.id };
+          })
+          .catch((err) => {
+            console.log(err);
+            throw err;
+          });
       },
     },
 
@@ -64,6 +119,15 @@ app.use(
   })
 );
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@crud.pk39a.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
